184168ca0a90a17a983bfe3e1acbd059c4c7b184
mominulislamsharon <ilsamikchannel328250@gmail.com>
2024-12-07 02:26:52 +0600

    route change finished

--- src/modules/Bookshop/product.route.ts
+++ src/modules/Bookshop/product.route.ts
@@ -3,7 +3,7 @@ import { ProductController } from './product.controller';
 
 const router = express.Router();
 // CREATE book
-router.post('/create-product', ProductController.createProduct);
+router.post('/', ProductController.createProduct);
 
 // get all books 
 router.get('/', ProductController.getAllbooks);
